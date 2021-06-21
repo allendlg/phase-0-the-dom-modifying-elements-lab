928570e31b2cf14d2db6c19629f2da971943e881
Allen de la Garza <allen.delagarza01@gmail.com>
2021-06-21 11:09:47 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+const newHeader = document.createElement('h1')
+newHeader.setAttribute("id", "victory")
+newHeader.innerHTML = "allen is the champion"
+document.body.appendChild(newHeader)
+
+
+
+
+
+
+
